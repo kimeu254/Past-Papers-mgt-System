945d599499f2b01b0620b05f4f803ed311ad08bd
kimeu254 <jimkimeu254@gmail.com>
2022-09-16 11:12:28 +0300

    added a vuex store and axios

--- vue/src/main.js
+++ vue/src/main.js
@@ -1,5 +1,13 @@
 import { createApp } from 'vue'
-import './style.css'
 import App from './App.vue'
+import store from './store'
+import router from './router'
 
-createApp(App).mount('#app')
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-icons/font/bootstrap-icons.css'
+import 'bootstrap'
+
+createApp(App)
+    .use(store)
+    .use(router)
+    .mount('#app')
