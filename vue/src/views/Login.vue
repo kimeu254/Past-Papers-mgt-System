f547f940a98629ebad19e649769f45e1987be25d
kimeu254 <jimkimeu254@gmail.com>
2022-09-16 11:22:08 +0300

    added layouts

---
+++ vue/src/views/Login.vue
@@ -0,0 +1,29 @@
+<template>
+    <h3>Login</h3>
+    <hr />
+    <form>
+        <div class="form-group">
+            <label for="exampleInputEmail1">Email address</label>
+            <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Email">
+        </div>
+        <div class="form-group">
+            <label for="exampleInputPassword1">Password</label>
+            <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+        </div>
+        <button type="submit" class="btn btn-primary">Login</button>
+        <hr />
+        <router-link :to="{name: 'register'}">
+            <button type="button" class="btn btn-link">Register</button>
+        </router-link>    <button type="button" class="btn btn-link">Reset Password</button>
+    </form>
+</template>
+
+<script>
+export default {
+    name: "Login"
+}
+</script>
+
+<style scoped>
+
+</style>
